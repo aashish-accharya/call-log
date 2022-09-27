c659f5099c52cbf7f8b4b8067a31a467fba6b3a4
aashish-acharya <aashish.acharya006@gmail.com>
2022-09-27 15:00:06 +0545

    Updated Auth Odoo 10

--- flasktest.py
+++ flasktest.py
@@ -1,50 +1,67 @@
-from flask import Flask,jsonify
-from flask_restful import Resource, Api
+from flask import Flask,jsonify, request
+from flask_restful import Api
 from xmlrpc import client as xmlrpclib
-import json
+import base64
 
 app = Flask(__name__)
 api = Api(app)
 
 url = 'http://192.168.2.123:8069'
 db = 'odoo'
-username = 'admin'
-password = 'admin'
+username=''
+password=''
 
-# data = {'called_from': 1,
-#         'called_to': '12345',
-#         'call_type': 'incoming',
-#         'call_site': 2,
-#         'call_date': '2022-01-01 00:00:00',
-#         'call_duration': 60,
-#         'call_remarks': 'Test',
-#     }
 
 common = xmlrpclib.ServerProxy('{}/xmlrpc/2/common'.format(url))
-uid = common.authenticate(db, username, password, {})
 models = xmlrpclib.ServerProxy('{}/xmlrpc/2/object'.format(url))
 
-@app.route('/create/<file>', methods=['POST'])
-def create(file):
-    with open (file) as f:
-        data = json.load(f)
+def getUid():
+    uid = common.authenticate(db, getUsername(), getPassword(), {})
+    if not uid:
+        raise Exception('Authentication failed')
+    return uid
+
+def getUsername():
+    header = request.headers.get('Authorization')
+    user=base64.b64decode(header.split(' ')[1])
+    username=user.decode('utf-8').split(':')[0]
+    return username
+
+def getPassword():
+    header = request.headers.get('Authorization')
+    user=base64.b64decode(header.split(' ')[1])
+    password=user.decode('utf-8').split(':')[1]
+    return password
+
+@app.route('/create', methods=['POST'])
+def create():
+    data = request.get_json()
+    password = getPassword()
+    uid = getUid()
     models.execute_kw(db, uid, password,'call.log', 'create',data)
     return jsonify(data)
 
 @app.route('/search/<field>,<value>')
 def search(field, value):
+    password = getPassword()
+    uid = getUid()
     yo = models.execute_kw(db, uid, password,"call.log", "search_read", [[[field, "=", value]]])
     return jsonify(yo)
 
-@app.route('/delete/<value>')
+@app.route('/delete/<value>', methods=['DELETE'])
 def delete(value):
+    password = getPassword()
+    uid = getUid()
     data = models.execute_kw(db, uid, password,'call.log', 'unlink', [value])
     return jsonify(data)
 
 @app.route('/get')
 def get():
+    password = getPassword()
+    uid = getUid()
     data = models.execute_kw(db, uid, password,'call.log', 'fields_get', [["called_from", "called_to", "call_type", "call_site", "call_date", "call_duration", "call_remarks"]])
     return jsonify(data)
 
 if __name__ == '__main__':
     app.run(debug=True)
+
